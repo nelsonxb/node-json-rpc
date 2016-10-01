cd9388ab5efdc3da1c860b25cc10a0ceecebbd12
Nelson Crosby <nc@sourcecomb.com>
2016-10-01 23:00:47 +1300

    Elaborate on basic info for docs in index

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // JSON-RPC for Node
 // =================
 //
+// _[GitHub](https://github.com/NelsonCrosby/node-json-rpc)_
+//
 // `node-json-rpc` is an RPC library for Node.js. It is designed to have a
 // simple API, and be adaptable to multiple transports.
+//
+// This module implements [JSON-RPC 2.0][spec] (_note: it currently lacks
+// support for batch requests_).
+//
+// See [the core docs](core.html) for the basic API, and the following docs
+// for transport specifics.
+//
+// - [TCP (and Unix Socket)](tcp.html)
+//
+// [spec]: http://www.jsonrpc.org/specification
 module.exports = require('./core')
 require('./tcp')
